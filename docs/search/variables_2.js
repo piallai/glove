b2a28daefab860d341acb3debfecf081f4a07bc2
piallai <59734081+piallai@users.noreply.github.com>
2025-11-30 21:40:27 +0100

    updates

--- docs/search/variables_2.js
+++ docs/search/variables_2.js
@@ -1,6 +1,8 @@
 var searchData=
 [
-  ['combo_5flist_0',['combo_list',['../class_glv_list_dialog__base.html#a4892fd78987f00f84cd6b0f40befdd4e',1,'GlvListDialog_base']]],
-  ['connector_1',['connector',['../class_glv_widget__base.html#a34db25fd6aa34e884974efdac69c4f64',1,'GlvWidget_base']]],
-  ['current_5findex_2',['current_index',['../class_glv_item_delegate__base.html#af13f36456302dc97058f12b845bae092',1,'GlvItemDelegate_base']]]
+  ['cli_5farguments_5fline_0',['CLI_arguments_line',['../class_glv_parameters_widget__base.html#ab47ed8e1a1f933278a312983b853dd3e',1,'GlvParametersWidget_base']]],
+  ['cli_5fparameters_1',['CLI_parameters',['../class_glv_parameters_widget__base.html#a214b7f0b3d6a117f3eae99eaa77d77ee',1,'GlvParametersWidget_base']]],
+  ['combo_5flist_2',['combo_list',['../class_glv_list_dialog__base.html#a4892fd78987f00f84cd6b0f40befdd4e',1,'GlvListDialog_base']]],
+  ['connector_3',['connector',['../class_glv_widget__base.html#a34db25fd6aa34e884974efdac69c4f64',1,'GlvWidget_base']]],
+  ['current_5findex_4',['current_index',['../class_glv_item_delegate__base.html#af13f36456302dc97058f12b845bae092',1,'GlvItemDelegate_base']]]
 ];
